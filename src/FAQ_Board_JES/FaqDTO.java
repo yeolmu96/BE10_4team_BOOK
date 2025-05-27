5675cf267ad3a28d7130343f077dae00cece336c
eunsun <jeong7653@naver.com>
2025-05-27 17:22:30 +0900

    faq board finished

---
+++ src/FAQ_Board_JES/FaqDTO.java
@@ -0,0 +1,63 @@
+package store;
+
+import java.util.Date;
+
+public class FaqDTO {
+	private int id;
+	private String title;
+	private String content;
+	private Date created_at;
+	
+	public FaqDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public FaqDTO(int id, String title, String content, Date created_at) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.content = content;
+		this.created_at = created_at;
+	}
+	public FaqDTO(int id, String title, String content) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.content = content;
+	}
+	public FaqDTO(int id, String title) {
+		super();
+		this.id = id;
+		this.title = title;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getCreated_at() {
+		return created_at;
+	}
+	public void setCreated_at(Date created_at) {
+		this.created_at = created_at;
+	}
+	
+	@Override
+	public String toString() {
+		return "FaqDTO [id=" + id + ", title=" + title + ", content=" + content + ", created_at=" + created_at + "]";
+	}
+}
