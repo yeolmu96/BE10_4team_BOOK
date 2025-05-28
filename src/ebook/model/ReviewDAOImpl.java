26bba18c4e4b642462f68266b774e347a05d7912
hi:9 <99hello.0.k@gmail.com>
2025-05-28 11:21:37 +0900

    리뷰DTO와 리뷰

---
+++ src/ebook/model/ReviewDAOImpl.java
@@ -0,0 +1,52 @@
+package ebook.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ReviewDAOImpl {
+	// DB 연결 메서드
+    private Connection getConnection() throws SQLException {
+        String url = "jdbc:mysql://192.168.0.114:3306/ebook_store?serverTimezone=UTC";
+        String user = "team4";
+        String pw = "team4";
+        return DriverManager.getConnection(url, user, pw);
+    }
+
+	// 리뷰 조회(select)
+    public ArrayList<ReviewDTO> getReview() {
+    	ArrayList<ReviewDTO> reviewlist = new ArrayList<ReviewDTO>();
+    	String sql = "select * from review";
+    	Connection con = null;
+    	PreparedStatement stmt = null;
+    	ResultSet rs = null;
+    	try {
+    		con = getConnection();
+    		stmt = con.prepareStatement(sql);
+    		rs = stmt.executeQuery();
+    		while(rs.next()) {
+    			ReviewDTO review = new ReviewDTO(rs.getString("id"), rs.getString("title"), rs.getInt("grade"), rs.getString("txt"));
+    			reviewlist.add(review);
+    		}
+    	}catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (stmt != null)
+					stmt.close();
+				if (con != null)
+					con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+    	return reviewlist;
+    	
+    }
+ 	
+}
---
+++ src/ebook/model/ReviewDTO.java
@@ -0,0 +1,52 @@
+package ebook.model;
+
+public class ReviewDTO {
+	private String id; // 입력한 회원의 ID
+	private String title; // 리뷰하는 책 제목
+	private int grade; // 평점
+	private String txt; // 리뷰 내용
+	
+	public ReviewDTO() {
+		
+	}
+	public ReviewDTO(String id, String title, int grade, String txt) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.grade = grade;
+		this.txt = txt;
+	}
+	
+	@Override
+	public String toString() {
+		return "ReviewDTO [id=" + id + ", title=" + title + ", grade=" + grade + ", txt=" + txt + "]";
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	public String getTxt() {
+		return txt;
+	}
+	public void setTxt(String txt) {
+		this.txt = txt;
+	}
+	
+	
+	
+	
+}
